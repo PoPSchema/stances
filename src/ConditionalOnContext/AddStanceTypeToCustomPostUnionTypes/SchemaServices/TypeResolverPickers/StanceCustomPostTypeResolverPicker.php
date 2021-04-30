da89f36f731684559e7a865eee18aafe90df0e35
leoloso <leo@getpop.org>
2021-04-30 04:41:39 +0000

    Merge pull request #646 from leoloso/rename-conditional-namespaces
    Rename conditional namespaces

--- src/ConditionalOnContext/AddStanceTypeToCustomPostUnionTypes/SchemaServices/TypeResolverPickers/StanceCustomPostTypeResolverPicker.php
+++ src/ConditionalOnContext/AddStanceTypeToCustomPostUnionTypes/SchemaServices/TypeResolverPickers/StanceCustomPostTypeResolverPicker.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PoPSchema\Stances\ConditionalOnEnvironment\AddStanceTypeToCustomPostUnionTypes\SchemaServices\TypeResolverPickers;
+namespace PoPSchema\Stances\ConditionalOnContext\AddStanceTypeToCustomPostUnionTypes\SchemaServices\TypeResolverPickers;
 
 use PoPSchema\CustomPosts\TypeResolvers\CustomPostUnionTypeResolver;
 use PoPSchema\Stances\TypeResolverPickers\AbstractStanceTypeResolverPicker;
